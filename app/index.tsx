6a459bae073343dddef95e8f6647f109df2696ef
Pablo <pablolucio_@hotmail.com>
2025-10-22 09:29:27 -0300

    chore: configures nativetailwind

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
+import '../global.css';
 import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
+      <Text className="text-cyan-700 text-xl">Home Screen</Text>
       <Link href="/profile">Go to profile</Link>
     </View>
   );
